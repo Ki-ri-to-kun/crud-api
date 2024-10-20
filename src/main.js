ed8ecc6d55abb495ad25cf4359bbda8e44fed1da
stas <staskazakov9@gmail.com>
2024-10-20 07:28:13 +0600

    feat: subtask 4 - error handling done

--- src/main.js
+++ src/main.js
@@ -9,45 +9,61 @@ const server = http.createServer((req, res) => {
   urlPaths.splice(0, 1);
     
   if(urlPaths[0] === 'api' && urlPaths[1] === 'users' && req.method === 'GET'){
-    
-    // get user by id
-    if(urlPaths.length === 3){
-      const id = urlPaths[2];
-      const user = users.find((u) => u.id === id);
+    try {
+       // get user by id
+      if(urlPaths.length === 3){
+        const id = urlPaths[2];
+        const user = users.find((u) => u.id === id);
       if(user){
         res.end(JSON.stringify(user));
       } else {
         res.statusCode = 404;
         res.end('404 - Not Found');
       }
-    // get all users
-    } else if(urlPaths.length === 2){
-       res.end(JSON.stringify(users));
-    } else {
-      res.statusCode = 404;
-      res.end('404 - Not Found');
-    }
+      // get all users
+      } else if(urlPaths.length === 2){
+        res.end(JSON.stringify(users));
+      } else {
+        res.statusCode = 404;
+        res.end('404 - Not Found');
+      }
+   } catch(err){
+      res.statusCode = 500;
+      res.end('500 - Internal Server Error');
+   }
+   
   } else if(urlPaths[0] === 'api' && urlPaths[1] === 'users' && req.method === 'POST'){
     
     req.on('data', (data) => {
-      const userData = JSON.parse(data.toString());
-      const newUser = {
-        id: uuid(),
-        ...userData
-      };
-      users.push(newUser);
-      res.statusCode = 201;
-      res.end(JSON.stringify(newUser));
+      try{
+         const userData = JSON.parse(data.toString());
+         const newUser = {
+          id: uuid(),
+          ...userData
+         };
+         users.push(newUser);
+         res.statusCode = 201;
+         res.end(JSON.stringify(newUser));
+      } catch(err){
+         res.statusCode = 500;
+         res.end('500 - Internal Server Error');
+      } 
+    });
+    
+    req.on('error', (error) => {
+      res.statusCode = 500;
+      res.end('500 - Internal Server Error');
     });
     
   } else if(urlPaths[0] === 'api' && urlPaths[1] === 'users' && urlPaths.length === 3 && req.method === 'PUT'){
     
       req.on('data', (data) => {
-         const id = urlPaths[2];
-         const newUser = JSON.parse(data.toString());
-         const index = users.findIndex((user) => user.id === id);
+        try{
+          const id = urlPaths[2];
+          const newUser = JSON.parse(data.toString());
+          const index = users.findIndex((user) => user.id === id);
          
-         if(index !== -1){
+          if(index !== -1){
             const currentUser = users[index];
             const updatedUser = {
               ...currentUser,
@@ -55,25 +71,39 @@ const server = http.createServer((req, res) => {
             };
             users[index] = updatedUser;
             res.end(JSON.stringify(updatedUser));   
-         } else {
+          } else {
             res.statusCode = 404;
             res.end('404 - Not Found');
+          }
+        } catch(err){
+          res.statusCode = 500;
+          res.end('500 - Internal Server Error');
         }
       });
+      
+     req.on('error', (error) => {
+      res.statusCode = 500;
+      res.end('500 - Internal Server Error');
+     });
 
   } else if(urlPaths[0] === 'api' && urlPaths[1] === 'users' && urlPaths.length === 3  && req.method === 'DELETE'){
-    const id = urlPaths[2];
     
-    const index = users.findIndex((user) => user.id === id);
+    try {
+      const id = urlPaths[2];
+      const index = users.findIndex((user) => user.id === id);
     
-     if(index !== -1){
+      if(index !== -1){
         users.splice(index, 1);
         res.statusCode = 204;
         res.end();
-     } else {
-            res.statusCode = 404;
-            res.end('404 - Not Found');
-     }
+      } else {
+        res.statusCode = 404;
+        res.end('404 - Not Found');
+      }
+    } catch(err){
+        res.statusCode = 500;
+        res.end('500 - Internal Server Error');
+    }
   
   } else {
     res.statusCode = 404;
